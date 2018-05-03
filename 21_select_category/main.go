3ebe8ae1cb093245fcbe500a5a4c5ae8e8a966ed
toversus <toversus26@gmail.com>
2018-05-03 01:32:14 +0000

    add knock21

---
+++ 21_select_category/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	var (
+		filePath string
+		keyword  string
+	)
+	flag.StringVar(&filePath, "file", "", "specify a file path")
+	flag.StringVar(&filePath, "f", "", "specify a file path")
+	flag.StringVar(&keyword, "key", "", "specify a keyword for filtering a wiki page")
+	flag.Parse()
+
+	articles, err := readJSON(filePath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, result := range articles.find(keyword).selectCategory() {
+		fmt.Printf("%#v\n", result)
+	}
+}
+
+// Article represents the body of Wiki page and its Title.
+type Article struct {
+	Title string `json:"title"`
+	Text  string `json:"text"`
+}
+
+// newArticle is a constructer of Article.
+func newArticle(a Article) Article {
+	return Article{a.Title, a.Text}
+}
+
+// Articles represents the slice of Article.
+type Articles []Article
+
+// readJSON reads/parses the json file and initiates Articles instance
+func readJSON(path string) (Articles, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not open a file specified: %s\n  %s", path, err)
+	}
+	defer f.Close()
+
+	reader := bufio.NewReader(f)
+	article, articles := Article{}, Articles{}
+	for {
+		buf, readErr := reader.ReadBytes('\n')
+		if (readErr != nil) && (readErr != io.EOF) {
+			return nil, fmt.Errorf("could not read a file content: %s", readErr)
+		}
+		if err = json.Unmarshal(buf, &article); err != nil {
+			fmt.Printf("could not parse json file: %s", err)
+			break
+		}
+
+		articles = append(articles, article)
+
+		if readErr == io.EOF {
+			break
+		}
+	}
+	return articles, nil
+}
+
+// find returns Articles filtered by specified keyword.
+func (articles Articles) find(keyword string) Articles {
+	var filtered Articles
+	for _, a := range articles {
+		if strings.Contains(a.Title, keyword) {
+			filtered = append(filtered, a)
+		}
+	}
+	return filtered
+}
+
+var reg = regexp.MustCompile(`\[\[Category:[^]]+\]\]`)
+
+// selectCategory returns Category description.
+func (articles Articles) selectCategory() []string {
+	var selected []string
+	for _, a := range articles {
+		for _, str := range reg.FindAllString(a.Text, -1) {
+			selected = append(selected, str)
+		}
+	}
+	return selected
+}
---
+++ 21_select_category/main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+var selectCategoryTests = []struct {
+	name    string
+	file    string
+	text    string
+	keyword string
+	expect  []string
+}{
+	{
+		name: "should return 5 categolized lines",
+		file: "test.txt",
+		text: `{"text": "インドです。\nイギリスに関係します。\n[[Category:インド|*]]\n[[Category:イギリス連邦]]", "title":"インド"}
+{"text": "イギリスです。\n[[Category:イギリス|*]]\n[[Category:英連邦王国|*]]\n[[Category:海洋国家]]", "title":"イギリス"}`,
+		keyword: "イギリス",
+		expect: []string{
+			"[[Category:イギリス|*]]",
+			"[[Category:英連邦王国|*]]",
+			"[[Category:海洋国家]]",
+		},
+	},
+	{
+		name:    "should return empty categories",
+		file:    "test.txt",
+		text:    `{"text": "インドです。\nイギリスに関係します。\n[[Category:Category:]]\n[[Category: [[Sub Category]]]]\n[[Category:インド|*]]\n[[Category:イギリス連邦]][[foobar]]\n[[Category:連邦]]]]", "title":"インド"}`,
+		keyword: "イギリス",
+		expect:  nil,
+	},
+}
+
+func TestSelectCategory(t *testing.T) {
+	for _, testcase := range selectCategoryTests {
+		t.Log(testcase.name)
+
+		f, err := os.Create(testcase.file)
+		if err != nil {
+			t.Errorf("could not create a file: %s\n  %s\n", testcase.file, err)
+		}
+		f.WriteString(testcase.text)
+		f.Close()
+
+		artists, err := readJSON(testcase.file)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if results := artists.find(testcase.keyword).selectCategory(); !reflect.DeepEqual(results, testcase.expect) {
+			t.Errorf("result => %#v\n should contain => %#v\n", results, testcase.expect)
+		}
+
+		if err := os.Remove(testcase.file); err != nil {
+			t.Errorf("could not delete a file: %s\n  %s\n", testcase.file, err)
+		}
+	}
+}
