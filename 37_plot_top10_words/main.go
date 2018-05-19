7559202ddf7ce201ae604341b38fcf7c884b1803
toversus <toversus26@gmail.com>
2018-05-19 11:43:55 +0900

    add knock37

---
+++ 37_plot_top10_words/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strings"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
+)
+
+func main() {
+	var filePath string
+	flag.StringVar(&filePath, "file", "", "specify a file path")
+	flag.StringVar(&filePath, "f", "", "specify a file path")
+	flag.Parse()
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot find the specified file: %s\n  %s\n", filePath, err)
+	}
+	defer f.Close()
+
+	morphs, err := newMorpheme(f)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	morphs.sortByAppearance().drawBarCharts("top10-commonly-used-words.png")
+}
+
+// morpheme represents the mapping list of MeCab format.
+type morpheme map[string]string
+
+// morphemes represents lists of Morpheme.
+type morphemes []*morpheme
+
+// newMorpheme implements the constructor of Morpheme.
+// MeCab outputs the following data structure through morphological analysis.
+// <Surface>\t<POS>,<POS subtyping1>,<POS subtyping2>,<POS subtyping3>,<Conjugation Form>,<Conjugation>,<Base>,<Furigana>,<Pronunciation>
+func newMorpheme(r io.Reader) (morphemes, error) {
+	morphs := morphemes{}
+	sc := bufio.NewScanner(r)
+	for sc.Scan() {
+		if sc.Text() == "EOS" || sc.Text() == "" {
+			continue
+		}
+		// separation of the surface field and the rest of the fields.
+		surf := strings.Split(sc.Text(), "\t")
+		// separation of the rest of the fields.
+		other := strings.Split(surf[1], ",")
+		morphs = append(morphs, &morpheme{
+			"surface": surf[0],
+			"base":    other[6],
+			"pos":     other[0],
+			"pos1":    other[1],
+		})
+	}
+	return morphs, nil
+}
+
+// sortCounter is used for sorting the key by counts
+type sortCounter struct {
+	Key   string
+	Count int
+}
+
+// countSorsortCountersters represents the slice of Countsorter
+type sortCounters []sortCounter
+
+func (counters sortCounters) String() string {
+	var buf bytes.Buffer
+	for _, counter := range counters {
+		// remove the first character "{"
+		ans := fmt.Sprintf("%+v\n", counter)[1:]
+		buf.WriteString(strings.Replace(ans, "}", "", -1))
+	}
+	return strings.TrimRight(buf.String(), "\n")
+}
+
+// sortByAppearance returns the surface verb stably sorted by their appearance
+func (morphes *morphemes) sortByAppearance() sortCounters {
+	// counter counts frequency of word
+	counter := map[string]int{}
+	var sortedWords sortCounters
+	var word string
+
+	// Use separate slice container to memorize stable iteration order
+	// See Iteration order section in the following URL:
+	// https://blog.golang.org/go-maps-in-action
+	var keys []string
+
+	for _, morph := range *morphes {
+		word = (*morph)["surface"]
+		if _, ok := counter[word]; ok {
+			counter[word]++
+			continue
+		}
+		// Memorize original order of words
+		keys = append(keys, word)
+		counter[word] = 1
+	}
+
+	for _, key := range keys {
+		// Assign sorter in fixed order using memorized keys
+		sortedWords = append(sortedWords, sortCounter{
+			Key:   key,
+			Count: counter[key],
+		})
+	}
+
+	sort.SliceStable(sortedWords, func(i, j int) bool {
+		return sortedWords[i].Count > sortedWords[j].Count
+	})
+
+	return sortedWords
+}
+
+// drawBarCharts draws bar charts
+func (counters sortCounters) drawBarCharts(filename string) error {
+	group := plotter.Values{}
+	var labels []string
+
+	p, err := plot.New()
+	if err != nil {
+		return fmt.Errorf("could not create a new plot: %s", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		group = append(group, float64(counters[i].Count))
+		labels = append(labels, counters[i].Key)
+	}
+
+	p.Title.Text = "Top 10 commonly used words"
+	p.Y.Label.Text = "counts"
+
+	w := vg.Points(10)
+
+	bars, err := plotter.NewBarChart(group, w)
+	if err != nil {
+		return fmt.Errorf("could not create a new bar chart: %s", err)
+	}
+	bars.LineStyle.Width = vg.Length(0)
+	bars.Color = plotutil.Color(0)
+
+	p.Add(bars)
+	p.NominalX(labels...)
+
+	if err := p.Save(5*vg.Inch, 3*vg.Inch, filename); err != nil {
+		return fmt.Errorf("could not save a bar chart: %s", err)
+	}
+
+	return nil
+}
