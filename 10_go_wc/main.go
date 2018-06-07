16fb189cd2badf8a47ffcb900b2b375c5c85a75e
toversus <toversus2357@gmail.com>
2018-06-07 20:47:35 +0900

    refactor test code on knock10

--- 10_go_wc/main.go
+++ 10_go_wc/main.go
@@ -14,12 +14,13 @@ func main() {
 	flag.StringVar(&filePath, "f", "", "specify a file path")
 	flag.Parse()
 
-	if _, err := os.Stat(filePath); err != nil {
-		fmt.Fprintf(os.Stderr, "could not find a file:\n  %s\n", err)
-		os.Exit(1)
+	f, err := os.Open(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not open a file: %s\n  %s", filePath, err)
 	}
+	defer f.Close()
 
-	count, err := countLineByScanner(filePath)
+	count, err := countLineByScanner(f)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -28,14 +29,8 @@ func main() {
 }
 
 // countLine counts lines of input file text.
-func countLineByScanner(path string) (int, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return -1, fmt.Errorf("could not open a file: %s\n  %s", path, err)
-	}
-	defer f.Close()
-
-	sc := bufio.NewScanner(f)
+func countLineByScanner(r io.Reader) (int, error) {
+	sc := bufio.NewScanner(r)
 	count := 0
 	for sc.Scan() {
 		count++
@@ -44,20 +39,13 @@ func countLineByScanner(path string) (int, error) {
 }
 
 // countLine counts lines of input file text.
-func countLineByReadLine(path string) (int, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return -1, fmt.Errorf("could not open a file: %s\n  %s", path, err)
-	}
-	defer f.Close()
-
-	r := bufio.NewReader(f)
+func countLineByReadLine(r io.Reader) (int, error) {
+	reader := bufio.NewReader(r)
 	count := 0
 	for {
-		_, _, err := r.ReadLine()
+		_, _, err := reader.ReadLine()
 		if (err != nil) && (err != io.EOF) {
-			fmt.Println(err)
-			os.Exit(1)
+			return 0, fmt.Errorf("could not read a line: %s", err)
 		}
 		if err == io.EOF {
 			break
--- 10_go_wc/main_test.go
+++ 10_go_wc/main_test.go
@@ -1,34 +1,31 @@
 package main
 
 import (
-	"os"
 	"reflect"
+	"strings"
 	"testing"
 )
 
 var countLineTests = []struct {
-	name    string
-	content string
-	file    string
-	expect  int
+	name   string
+	text   string
+	expect int
 }{
 	{
 		name: "should return the number of lines from full text",
-		content: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+		text: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
 
 		Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
 		
 		Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
 		
 		Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex`,
-		file:   "full-text.txt",
 		expect: 7,
 	},
 	{
-		name:    "should return 0 due to the empty file",
-		content: ``,
-		file:    "empty-text.txt",
-		expect:  0,
+		name:   "should return 0 due to the empty file",
+		text:   ``,
+		expect: 0,
 	},
 }
 
@@ -36,62 +33,50 @@ func TestCountLineByScanner(t *testing.T) {
 	for _, testcase := range countLineTests {
 		t.Log(testcase.name)
 
-		f, err := os.Create(testcase.file)
-		if err != nil {
-			t.Errorf("could not create a file: %s\n  %s\n", testcase.file, err)
-		}
-		f.WriteString(testcase.content)
-
-		result, err := countLineByScanner(testcase.file)
+		r := strings.NewReader(testcase.text)
+		result, err := countLineByScanner(r)
 		if err != nil {
 			t.Error(err)
 		}
 		if !reflect.DeepEqual(result, testcase.expect) {
 			t.Errorf("result => %#v\n expect => %#v", result, testcase.expect)
 		}
-		f.Close()
-
-		if err := os.Remove(testcase.file); err != nil {
-			t.Errorf("could not delete a file: %s\n  %s\n", testcase.file, err)
-		}
 	}
 }
 
+var result int
+
 func BenchmarkCountLineByScanner(b *testing.B) {
+	var n int
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		countLineByScanner("./hightemp.txt")
+		r := strings.NewReader(countLineTests[0].text)
+		n, _ = countLineByScanner(r)
 	}
+	result = n
 }
 
 func TestCountLineByReadLine(t *testing.T) {
 	for _, testcase := range countLineTests {
 		t.Log(testcase.name)
 
-		f, err := os.Create(testcase.file)
-		if err != nil {
-			t.Errorf("could not create a file: %s\n  %s\n", testcase.file, err)
-		}
-		f.WriteString(testcase.content)
-
-		result, err := countLineByReadLine(testcase.file)
+		r := strings.NewReader(testcase.text)
+		result, err := countLineByReadLine(r)
 		if err != nil {
 			t.Error(err)
 		}
 		if !reflect.DeepEqual(result, testcase.expect) {
 			t.Errorf("result => %#v\n expect => %#v", result, testcase.expect)
 		}
-		f.Close()
-
-		if err := os.Remove(testcase.file); err != nil {
-			t.Errorf("could not delete a file: %s\n  %s\n", testcase.file, err)
-		}
 	}
 }
 
 func BenchmarkCountLineByReadLine(b *testing.B) {
+	var n int
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		countLineByReadLine("./hightemp.txt")
+		r := strings.NewReader(countLineTests[0].text)
+		n, _ = countLineByReadLine(r)
 	}
+	result = n
 }
