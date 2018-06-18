ad43d27b1a020e6f9ff444e4d78b50395b43c121
toversus <toversus2357@gmail.com>
2018-06-18 21:54:08 +0900

    add knock67

---
+++ 67_retrieve_multiple_documents/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Artist struct {
+	ID       int      `json:"id"`
+	GID      string   `json:"gid"`
+	Name     string   `json:"name"`
+	SortName string   `json:"sort_name"`
+	Area     string   `json:"area"`
+	Aliases  []*Alias `json:"aliases"`
+	Begin    *Begin   `json:"begin"`
+	End      *End     `json:"end"`
+	Tags     []*Tag   `json:"tags"`
+	Rating   *Rating  `json:"rating"`
+}
+
+type Artists []*Artist
+
+type Alias struct {
+	Name     string `json:"name"`
+	SortName string `json:"sort_name"`
+}
+
+type Begin struct {
+	Year  int `json:"year"`
+	Month int `json:"month"`
+	Date  int `json:"date"`
+}
+
+type End struct {
+	Year  int `json:"year"`
+	Month int `json:"month"`
+	Date  int `json:"date"`
+}
+
+type Tag struct {
+	Count int    `json:"count"`
+	Value string `json:"value"`
+}
+
+type Rating struct {
+	Count int `json:"count"`
+	Value int `json:"value"`
+}
+
+func main() {
+	var alias string
+	flag.StringVar(&alias, "alias", "", "specify the artist name")
+	flag.Parse()
+
+	session, err := mgo.Dial("mongodb://localhost")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to connect to mongodb://localhost:\n  %s", err)
+		os.Exit(1)
+	}
+
+	c := session.DB("MusicBrainz").C("artist")
+
+	var artists Artists
+	if err = c.Find(bson.M{"aliases.name": alias}).All(&artists); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to find specified alias of artist's name:\n  %s", err)
+		os.Exit(1)
+	}
+	for _, artist := range artists {
+		fmt.Println(artist)
+	}
+}
+
+func readBSON(r io.Reader) (Artists, error) {
+	var artists Artists
+	reader := bufio.NewReader(r)
+	for {
+		artist := Artist{}
+		buf, readErr := reader.ReadBytes('\n')
+		if (readErr != nil) && (readErr != io.EOF) {
+			return nil, fmt.Errorf("could not read json file:\n  %s", readErr)
+		}
+
+		if err := bson.UnmarshalJSON(buf, &artist); err != nil && readErr != io.EOF {
+			return nil, fmt.Errorf("could not parse json format:\n  %s", err)
+		}
+
+		artists = append(artists, &artist)
+
+		if readErr == io.EOF {
+			break
+		}
+	}
+	return artists, nil
+}
