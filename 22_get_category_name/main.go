3def11cd1cee129edcef279a9dcdcc663c7d2dfe
toversus <toversus2357@gmail.com>
2018-07-05 22:18:50 +0900

    refactor code on knock22

--- 22_get_category_name/main.go
+++ 22_get_category_name/main.go
@@ -21,9 +21,16 @@ func main() {
 	flag.StringVar(&keyword, "key", "", "specify a keyword for filtering a wiki page")
 	flag.Parse()
 
-	articles, err := readJSON(filePath)
+	f, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintf(os.Stderr, "could not get the list of articles: %s\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	articles, err := readJSON(f)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get the list of articles: %s\n", err)
 		os.Exit(1)
 	}
 
@@ -43,23 +50,16 @@ type Article struct {
 type Articles []Article
 
 // readJSON reads/parses the json file and initiates Articles instance
-func readJSON(path string) (Articles, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, fmt.Errorf("could not open a file specified: %s\n  %s", path, err)
-	}
-	defer f.Close()
-
-	reader := bufio.NewReader(f)
+func readJSON(r io.Reader) (Articles, error) {
+	reader := bufio.NewReader(r)
 	article, articles := Article{}, Articles{}
 	for {
 		buf, readErr := reader.ReadBytes('\n')
 		if (readErr != nil) && (readErr != io.EOF) {
 			return nil, fmt.Errorf("could not read a file content: %s", readErr)
 		}
-		if err = json.Unmarshal(buf, &article); err != nil {
-			fmt.Printf("could not parse json file: %s", err)
-			break
+		if err := json.Unmarshal(buf, &article); err != nil {
+			return nil, fmt.Errorf("could not parse json file: %s", err)
 		}
 
 		articles = append(articles, article)
@@ -68,6 +68,7 @@ func readJSON(path string) (Articles, error) {
 			break
 		}
 	}
+
 	return articles, nil
 }
 
@@ -86,11 +87,11 @@ var reg = regexp.MustCompile(`\[\[Category:([^|]]+)(?:|.+)?\]\]`)
 
 // getCategoryName returns strings containing the Category name declared in the line
 func (articles Articles) getCategoryName() []string {
-	selected := []string{}
+	var categories []string
 	for _, a := range articles {
 		for _, line := range reg.FindAllStringSubmatch(a.Text, -1) {
-			selected = append(selected, line[1])
+			categories = append(categories, line[1])
 		}
 	}
-	return selected
+	return categories
 }
--- 22_get_category_name/main_test.go
+++ 22_get_category_name/main_test.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-	"os"
 	"reflect"
+	"strings"
 	"testing"
 )
 
 var getCategoryNameTest = []struct {
 	name    string
-	file    string
 	text    string
 	keyword string
 	expect  []string
 }{
 	{
-		name: "should return 5 category names",
-		file: "test.txt",
+		name: "should return 3 category names",
 		text: `{"text": "インドです。\nイギリスに関係します。\n[[Category:インド|*]]\n[[Category:イギリス連邦]]", "title":"インド"}
 {"text": "イギリスです。\n[[Category:イギリス|イギリス]]\n[[Category:英連邦王国|*]]\n[[Category:海洋国家]]", "title":"イギリス"}`,
 		keyword: "イギリス",
@@ -31,55 +29,32 @@ func TestGetCategoryName(t *testing.T) {
 	for _, testcase := range getCategoryNameTest {
 		t.Log(testcase.name)
 
-		f, err := os.Create(testcase.file)
-		if err != nil {
-			t.Errorf("could not create a file: %s\n  %s\n", testcase.file, err)
-		}
-		f.WriteString(testcase.text)
-		f.Close()
-
-		articles, err := readJSON(testcase.file)
+		r := strings.NewReader(testcase.text)
+		filtered, err := readJSON(r)
 		if err != nil {
 			t.Error(err)
 		}
-
-		for i, result := range articles.find(testcase.keyword).getCategoryName() {
+		for i, result := range filtered.find(testcase.keyword).getCategoryName() {
 			if !reflect.DeepEqual(result, testcase.expect[i]) {
 				t.Errorf("result => %#v\n should contain => %#v\n", result, testcase.expect[i])
 			}
 		}
-
-		if err := os.Remove(testcase.file); err != nil {
-			t.Errorf("could not delete a file: %s\n  %s\n", testcase.file, err)
-		}
 	}
 }
 
-func BenchmarkGetCategoryName(b *testing.B) {
-	for _, testcase := range getCategoryNameTest {
-		f, err := os.Create(testcase.file)
-		if err != nil {
-			b.Errorf("could not create a file: %s\n  %s\n", testcase.file, err)
-		}
-		f.WriteString(testcase.text)
-		f.Close()
-	}
+var result []string
 
+func BenchmarkGetCategoryName(b *testing.B) {
+	var categories []string
 	for i := 0; i < b.N; i++ {
 		for _, testcase := range getCategoryNameTest {
-			articles, err := readJSON(testcase.file)
-			if err != nil {
-				b.Error(err)
-			}
+			b.StopTimer()
+			r := strings.NewReader(testcase.text)
+			filtered, _ := readJSON(r)
+			b.StartTimer()
 
-			articles.find(testcase.keyword).getCategoryName()
-
+			categories = filtered.find(testcase.keyword).getCategoryName()
 		}
 	}
-
-	for _, testcase := range getCategoryNameTest {
-		if err := os.Remove(testcase.file); err != nil {
-			b.Errorf("could not delete a file: %s\n  %s\n", testcase.file, err)
-		}
-	}
+	result = categories
 }
